2033fad3053bb2ecf3632e84fd250a7dc63a0de9
martinalejandrohakanson29-art <martinalejandrohakanson29@gmail.com>
2025-12-10 11:36:38 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 import { Card, CardContent } from "@/components/ui/card"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-
+export const dynamic = "force-dynamic";
 export default async function Home() {
   const carouselItems = await getCarouselItems()
   const products = await getProducts()

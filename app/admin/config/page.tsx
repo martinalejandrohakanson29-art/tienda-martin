f377ef173aebed561823e29b48b010aa23d141fa
martinalejandrohakanson29-art <martinalejandrohakanson29@gmail.com>
2025-12-10 21:50:26 -0300

    Update page.tsx

--- app/admin/config/page.tsx
+++ app/admin/config/page.tsx
@@ -1,1 +1,13 @@
+import { getConfig } from "@/app/actions/config"
+import ConfigClient from "./config-client"
 
+export const dynamic = "force-dynamic"
+
+export default async function AdminConfigPage() {
+    const config = await getConfig()
+    
+    // Serializamos para evitar problemas de fechas o tipos complejos
+    const initialConfig = JSON.parse(JSON.stringify(config))
+
+    return <ConfigClient initialConfig={initialConfig} />
+}

81fece2bf2ae8d23f5d471ae493b4e8f4606d6ad
martinalejandrohakanson29-art <martinalejandrohakanson29@gmail.com>
2025-12-16 20:30:20 -0300

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// 👇 Esta es la nueva función para dar formato a los precios
+export function formatPrice(price: number | string) {
+  return "$" + Number(price).toLocaleString('es-AR', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })
+}

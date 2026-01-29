53259b5d1d2170c811f000eb8923872b9d06e365
martinalejandrohakanson29-art <martinalejandrohakanson29@gmail.com>
2026-01-28 22:11:16 -0300

    Create s3.ts

---
+++ lib/s3.ts
@@ -0,0 +1,13 @@
+// lib/s3.ts
+import { S3Client } from "@aws-sdk/client-s3";
+
+export const s3Client = new S3Client({
+    region: process.env.S3_REGION || "auto",
+    endpoint: process.env.S3_ENDPOINT,
+    credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+    },
+    // Esto es necesario para que funcione con Railway/MinIO
+    forcePathStyle: true, 
+});
